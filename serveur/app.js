04e76d2e68708716bed7ce5e61bc92f298275e07
vrascle <victor.rascle@lyceeastier.com>
2024-01-17 17:18:13 +0100

    install mysql, cors pour l'api

--- serveur/app.js
+++ serveur/app.js
@@ -6,6 +6,7 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var  db_connex = require('./Bdd/db_connexion');
 const cors = require('cors')
 
 
