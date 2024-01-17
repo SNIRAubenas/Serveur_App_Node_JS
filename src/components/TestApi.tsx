b017c90fe0006a659a751268b0db5dd93133a4ca
Nathan Vernet <nathan.vernet@lyceeastier.com>
2024-01-17 17:19:03 +0100

    Test accès API

---
+++ src/components/TestApi.tsx
@@ -0,0 +1,27 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
+const TestApi = () => {
+    const [apiResponse, setApiResponse] = useState('');
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const response = await axios.get('http://10.0.0.156:3000/');
+                setApiResponse(response.data);
+            } catch (error) {
+                console.error('Erreur lors de la récupération des données de l\'API', error);
+            }
+        };
+
+        fetchData();
+    }, []); // Le tableau vide signifie que cet effet ne s'exécute qu'une fois après le montage initial du composant
+
+    return (
+        <div>
+            <p>Réponse de l'API : {apiResponse}</p>
+        </div>
+    );
+};
+
+export default TestApi;
