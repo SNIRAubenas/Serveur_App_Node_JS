6aab7c68f397d367912e04d7810db54f793bc14d
vrascle <victor.rascle@lyceeastier.com>
2024-02-14 17:16:12 +0100

    toutes les requette sql sont faite

--- serveur/Bdd/config.js
+++ serveur/Bdd/config.js
@@ -1,9 +1,9 @@
 const config = {
     //configuration de notre base de donnée
     db: {
-        host: "localhost",
+        host: "10.0.0.164",
         user: "root",
-        password: "root",
+        password: "",
         database: "table_de_tri",
         connectTimeout: 60000
     }
