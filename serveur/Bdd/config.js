c0ef223a2fda95bec4eebe5752f2bc597c726966
vrascle <victor.rascle@lyceeastier.com>
2024-03-13 15:15:55 +0100

    utilisateur correct

--- serveur/Bdd/config.js
+++ serveur/Bdd/config.js
@@ -1,9 +1,9 @@
 const config = {
     //configuration de notre base de donnée
     db: {
-        host: "10.0.0.164",
-        user: "root",
-        password: "",
+        host: "10.0.0.117",
+        user: "rat",
+        password: "NukeTown@07",
         database: "table_de_tri",
         connectTimeout: 60000
     }
